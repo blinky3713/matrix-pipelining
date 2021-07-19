6fb56e0a32099ede307497b91caff7d9ef81c377
blinky <martin@foam.space>
2021-07-19 18:59:47 +0000

    split and unsplit matrix

--- src/Example/Sized/Matrix.hs
+++ src/Example/Sized/Matrix.hs
@@ -2,40 +2,172 @@ module Example.Sized.Matrix where
 
 import Clash.Prelude hiding (transpose)
 
-type Matrix m n = Vec m (Vec n Int) -- n x m matrix
+type Matrix m n a = Vec m (Vec n a) -- n x m matrix
 
 dot
-  :: KnownNat n
+  :: forall n a.
+     KnownNat n
   => 1 <= n
-  => Vec n Int
-  -> Vec n Int
-  -> Int
+  => Num a
+  => Vec n a
+  -> Vec n a
+  -> a
 dot a b = sum $ zipWith (*) a b
 
 
 mvMult
-  :: KnownNat n
+  :: forall m n a.
+     KnownNat n
   => 1 <= n
-  => Matrix m n
-  -> Vec n Int
-  -> Vec m Int
+  => Num a
+  => Matrix m n a
+  -> Vec n a
+  -> Vec m a
 mvMult m v = map (dot v) m
 
 mmMult
-  :: KnownNat m
+  :: forall m n p a.
+     KnownNat m
   => KnownNat p
+  => Num a
   => 1 <= m
-  => Matrix n m
-  -> Matrix m p
-  -> Matrix n p
+  => Matrix n m a
+  -> Matrix m p a
+  -> Matrix n p a
 mmMult a b =
   map (mvMult $ transpose b) a
 
 transpose
-  :: KnownNat n
-  => Matrix m n -- matrix (n x m) of column vectors
-  -> Matrix n m -- matrix (m x n) of column vectors
+  :: forall m n a.
+     KnownNat n
+  => Num a
+  => Matrix m n a -- matrix (n x m) of column vectors
+  -> Matrix n m a -- matrix (m x n) of column vectors
 transpose Nil = repeat Nil
 transpose (Cons as rest) =
   let m = transpose rest
   in zipWith (:>) as m
+
+{-
+[ [a,b,c,d]
+, [d,e,f,g]
+, [i,j,k,l]
+, [m,n,o,p]
+]
+
+| a b c d |
+| e f g h |
+| i j k l |
+| m n o p |
+
+
+[ [[a,b],[e,f]]
+, [[c,d],[g,h]]
+, [[i,j],[k,l]]
+, [[m,n],[o,p]]
+]
+
+-> ||a b| |c d||
+   ||e f| |g h||
+   |           |
+   ||i j| |k l||
+   ||m n| |o p||
+-}
+
+
+splitMatrix
+  :: forall aa_m aa_n sm sn m n a.
+     KnownNat aa_m
+  => KnownNat aa_n
+  => KnownNat sm
+  => KnownNat sn
+  => 1 <= aa_m
+  => 1 <= sm
+  => (aa_m * sm) ~ m
+  => (aa_n * sn) ~ n
+  => SNat aa_m
+  -> SNat aa_n
+  -> SNat sm
+  -> SNat sn
+  -> Matrix m n a
+  -> Matrix aa_m aa_n (Matrix sm sn a)
+splitMatrix _ _ sm sn =
+  map (traverse $ unconcat sn) . unconcat sm
+
+unsplitMatrix
+  :: forall aa_m aa_n sm sn m n a.
+     KnownNat aa_m
+  => KnownNat aa_n
+  => KnownNat sm
+  => KnownNat sn
+  => 1 <= aa_n
+  => (aa_m * sm) ~ m
+  => (aa_n * sn) ~ n
+  => SNat aa_m
+  -> SNat aa_n
+  -> SNat sm
+  -> SNat sn
+  -> Matrix aa_m aa_n (Matrix sm sn a)
+  -> Matrix m n a
+unsplitMatrix _ _ _ _ = concatMap (map concat . sequenceA)
+
+type Counter aa_n bb_n aa_m = (Index aa_n, Index bb_n, Index aa_m)
+
+mmmult2d
+  :: forall a_m a_n b_m b_n aa_m aa_n bb_m bb_n aa_sm aa_sn bb_sm bb_sn.
+     KnownNat aa_m
+  => KnownNat aa_n
+  => KnownNat bb_m
+  => KnownNat bb_n
+  => KnownNat aa_sm
+  => KnownNat aa_sn
+  => KnownNat bb_sm
+  => KnownNat bb_sn
+  => 1 <= a_m
+  => 1 <= a_n
+  => 1 <= b_m
+  => 1 <= b_n
+  => a_n ~ b_m
+
+  -- Constrain submatrices:
+  => a_m ~ (aa_m * aa_sm)
+  => a_n ~ (aa_n * aa_sn)
+  => b_m ~ (bb_m * bb_sm)
+  => b_n ~ (bb_n * bb_sn)
+  => 1 <= aa_sm
+  => 1 <= aa_sn
+  => 1 <= bb_sm
+  => 1 <= bb_sn
+  => bb_sm ~ aa_sn
+
+  -- Allow user to pass submatrix sizes:
+  => SNat aa_m
+  -- ^ Number of rows in submatrix of AA
+  -> SNat aa_sn
+  -- ^ Number of columns in submatrix of AA
+  -> SNat bb_n
+  -- ^ Number of columns in submatrix of BB
+
+  -- Matrices to multiply:
+  -> Signal System (Maybe (Matrix a_m a_n Int, Matrix b_m b_n Int))
+
+  -- Result returned after calculating for a while:
+  -> Signal System (Maybe (Matrix a_m b_n Int))
+mmmult2d = undefined
+
+
+{-
+mealySource#
+
+:: (HiddenClockResetEnable dom, NFDataX s)
+=> (s -> i -> (s, o))
+Transfer function in mealy machine form: state -> input -> (newstate,output)
+
+-> s
+Initial state
+
+-> Signal dom i -> Signal dom o
+Synchronous sequential function with input and output matching that of the mealy machine
+
+Create a synchronous function from a combinational function describing a mealy machine
+-}
